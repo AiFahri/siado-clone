c7cd9267d527139af0c70355a7c2b0d432d04634
AiFahri <fahrinurilll@gmail.com>
2025-05-03 23:53:54 +0700

    initial commit

---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-purple-600">
+            <p className="text-4xl font-bold text-white">Welcome brok</p>
+        </div>
+    );
+}
+
