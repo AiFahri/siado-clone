e087ab547470638aca17425dc47e012707f88829
ayas <ahmdyaasiin@student.ub.ac.id>
2025-05-04 07:02:21 +0700

    feat: Create database migrations for core application models

---
+++ database/migrations/2025_05_03_192350_create_course_enrollments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at');
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('course_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_enrollments');
+    }
+};
