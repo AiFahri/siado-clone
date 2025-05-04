d650a560f228caf8a32698c69fce162e5414a0b1
ayas <ahmdyaasiin@student.ub.ac.id>
2025-05-04 07:03:51 +0700

    feat: Implement JWT authentication for signup and signin

---
+++ app/Http/Controllers/JWTAuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTAuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:50',
+            'email' => 'required|string|email|max:50|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+            'role' => 'student',
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('token'), 201);
+    }
+
+    public function signin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            return response()->json(compact('token'));
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\JWTAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('signup', [JWTAuthController::class, 'signup']);
+    Route::post('signin', [JWTAuthController::class, 'signin']);
+});
