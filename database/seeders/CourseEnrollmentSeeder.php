81eaafa7890e9093d4877a11ec8bc72b6cd90a44
ayas <ahmdyaasiin@student.ub.ac.id>
2025-05-06 00:25:07 +0700

    feat: Seed course enrollments for anakrajin and other users

--- database/seeders/CourseEnrollmentSeeder.php
+++ database/seeders/CourseEnrollmentSeeder.php
@@ -12,12 +12,22 @@ class CourseEnrollmentSeeder extends Seeder
      */
     public function run(): void
     {
+
+        // seeding anakrajin
+        foreach (range(1, 2) as $index) {
+            DB::table('course_enrollments')->insert([
+                'created_at' => now(),
+                'user_id' => 5,
+                'course_id' => $index
+            ]);
+        };
+
         $course_id = 1;
-        foreach (range(1, 11) as $index) {
+        foreach (range(1, 10) as $index) {
 
             DB::table('course_enrollments')->insert([
                 'created_at' => now(),
-                'user_id' => $index,
+                'user_id' => $index + 5,
                 'course_id' => $course_id
             ]);
 
