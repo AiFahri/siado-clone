05160e2604b7b402bd4ea10fe343012d49757386
ayas <ahmdyaasiin@student.ub.ac.id>
2025-05-06 00:24:38 +0700

    feat: Implement JWT authentication middleware

---
+++ app/Http/Middleware/JWTAuthentication.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTAuthentication
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            JWTAuth::parseToken()->authenticate();
+        } catch (TokenExpiredException $e) {
+            throw $e;
+        } catch (TokenInvalidException $e) {
+            throw $e;
+        } catch (JWTException $e) {
+            throw $e;
+        }
+
+        return $next($request);
+    }
+}
