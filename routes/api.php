91e474a65576db9bbee767a637e2a72f7dcab579
ayas <ahmdyaasiin@student.ub.ac.id>
2025-05-06 00:25:54 +0700

    feat: Implement course and assignment API endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,36 @@
 <?php
 
-use App\Http\Controllers\JWTAuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\JWTAuthController;
+use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers\SubmissionController;
 
 Route::prefix('auth')->group(function () {
     Route::post('signup', [JWTAuthController::class, 'signup']);
     Route::post('signin', [JWTAuthController::class, 'signin']);
+});
+
+Route::middleware('jwt.auth')->group(function () {
+    Route::prefix('/users/_self')->group(function () {
+        Route::get('', [JWTAuthController::class, 'me']);
+        Route::get('/courses', [CourseController::class, 'selfCourses']);
+        Route::get('/assignments', [AssignmentController::class, 'selfAssignments']);
+        Route::get('/submissions', [SubmissionController::class, 'selfSubmissions']);
+    });
+
+    Route::prefix('/courses')->group(function () {
+        Route::get('', [CourseController::class, 'all']);
+
+        Route::get('/{course}', [CourseController::class, 'get'])->where('course', '[0-9]+');
+        Route::post('/{course}', [CourseController::class, 'enroll'])->where('course', '[0-9]+');
+        Route::delete('/{course}', [CourseController::class, 'unenroll'])->where('course', '[0-9]+');
+
+        Route::middleware('enrolled')->get('/{course}/assignments', [AssignmentController::class, 'courseAssignment'])->where('course', '[0-9]+');
+
+        Route::middleware('enrolled')->get('/{course}/assignments/{assignment}/submissions', [SubmissionController::class, 'userAssignmentSubmission'])->where(['course' => '[0-9]+', 'assignment' => '[0-9]+']);
+        Route::middleware('enrolled')->post('/{course}/assignments/{assignment}/submissions', [SubmissionController::class, 'storeSubmission'])->where(['course' => '[0-9]+', 'assignment' => '[0-9]+']);
+        Route::middleware('enrolled')->patch('/{course}/assignments/{assignment}/submissions', [SubmissionController::class, 'updateSubmission'])->where(['course' => '[0-9]+', 'assignment' => '[0-9]+']);
+        Route::middleware('enrolled')->delete('/{course}/assignments/{assignment}/submissions', [SubmissionController::class, 'deleteSubmission'])->where(['course' => '[0-9]+', 'assignment' => '[0-9]+']);
+    });
 });
